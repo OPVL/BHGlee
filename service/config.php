4255f032a21529c05111fa51145c92f2d69c69f7
lloyd <lloydculpepper4@gmail.com>
2019-06-10 15:08:08 +0100

    initial commit

---
+++ service/config.php
@@ -0,0 +1,7 @@
+<?php
+
+define('OAUTH_URL', 'https://auth.bullhornstaffing.com/oauth');
+define('CLIENT_ID', '3b2ab272-af50-4098-a3b0-f8fe712c01e1');
+define('CLIENT_SECRET', 'bne5ncAwEMNVwbCfJ0EGq2HU');
+define('USERNAME', 'gleecall.api');
+define('PASSWORD', 'Arnie123!');
